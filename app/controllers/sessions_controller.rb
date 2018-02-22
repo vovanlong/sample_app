c46276358e8ce802bc7eaa353956fb4d24ad7bb9
vovanlong <vovanlong1997@gmail.com>
2018-02-22 16:27:32 +0700

    chap10

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,10 +3,10 @@ class SessionsController < ApplicationController
 
   def create
     @user = User.find_by email: params[:session][:email].downcase
-    if @user && @user.authenticate params[:session][:password]
+    if @user && @user.authenticate(params[:session][:password])
       log_in @user
       params[:session][:remember_me] == "1" ? remember(@user) : forget(@user)
-      redirect_to @user
+      redirect_back_or @user
     else
       flash.now[:danger] = t "sessions.create.invalid"
       render :new
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,14 @@
 class UsersController < ApplicationController
-  def show
-    @user = User.find_by id: params[:id]
+  before_action :logged_in_user, except: %i(show new create)
+  before_action :load_user, except: %i(index new create)
+  before_action :correct_user, only: %i(edit update)
+  before_action :admin_user, only: :destroy
+
+  def index
+    @users = User.paginate page: params[:page]
   end
+
+  def show; end
 
   def new
     @user = User.new
@@ -18,9 +25,48 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit; end
+
+  def update
+    return render :edit unless @user.update_attributes user_params 
+    flash[:success] = t "users.update.content"
+    redirect_to @user
+  end
+
+  def destroy
+    if @user.destroy
+      flash[:success] = t "users.destroy.content"
+    else
+      flash[:danger] = t "users.destroy.content_error"
+    end
+    redirect_to users_url
+  end
+
   private
 
   def user_params
     params.require(:user).permit :name, :email, :password, :password_confirmation
+  end
+
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = t "users.logged_in.content"
+      redirect_to login_url
+     end
+  end
+
+  def correct_user
+    load_user
+    redirect_to root_url unless current_user? @user
+  end
+
+  def admin_user
+    redirect_to root_url unless current_user.admin?
+  end
+
+  def load_user
+    @user = User.find_by id: params[:id]
+    @user || render(file: "public/404.html", status: 404, layout: true)
   end
 end
