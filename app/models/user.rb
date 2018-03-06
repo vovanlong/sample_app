ac7d25ea457807e3f5ba51930d43a7464a6c2152
vovanlong <vovanlong1997@gmail.com>
2018-03-06 10:18:25 +0700

    chap14

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
   has_many :microposts, dependent: :destroy
+  has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
   attr_accessor :remember_token, :activation_token, :reset_token
   validates :name,  presence: true, length: {maximum: Settings.name_maxlenth}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -60,7 +64,22 @@ class User < ApplicationRecord
   end
 
   def feed
-    microposts.swap
+    Micropost.find_post_of_current_and_followed(following_ids, id).swap
+  end
+
+  # Follows a user.
+  def follow other_user
+    following << other_user
+  end
+
+  # Unfollows a user.
+  def unfollow other_user
+    following.delete other_user
+  end
+
+  # Returns true if the current user is following the other user.
+  def following? other_user
+    following.include? other_user
   end
 
   private
